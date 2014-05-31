e864046a56eded0ab6a4af4b09b73bccb0e2bb73
Qwx <qwxmail@gmail.com>
2014-05-31 20:48:56 +0300

    Added Comparer

---
+++ BackMeUp/Comparer.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using BackMeUp.Utils;
+
+namespace BackMeUp
+{
+    public class Comparer
+    {
+        public bool CompareDirectories(string saveDir, string backupDir)
+        {
+            var saveFiles = Directory.GetFiles(saveDir);
+            var backupFiles = Directory.GetFiles(backupDir);
+
+            if (saveDir.Length != backupDir.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < saveFiles.Length; i++)
+            {
+                if (!CompareFiles(saveFiles[i], backupFiles[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool CompareFiles(string file1, string file2)
+        {
+            if (Path.GetFileName(file1) != Path.GetFileName(file2))
+            {
+                return false;
+            }
+
+            if (CalculateChecksum(file1) != CalculateChecksum(file2))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public ushort CalculateChecksum(string filePath)
+        {
+            byte[] bytes;
+            using (var file = new FileStream(filePath, FileMode.Open))
+            {
+                bytes = new byte[file.Length];
+                file.Read(bytes, 0, (int) file.Length);
+            }
+            return new Crc16().ComputeChecksum(bytes);
+        }
+    }
+}
---
+++ BackMeUp/Utils/Crc16.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BackMeUp.Utils
+{
+    public class Crc16
+    {
+        private const ushort Polynomial = 0xA001;
+        private readonly ushort[] _table = new ushort[256];
+
+        public ushort ComputeChecksum(byte[] bytes)
+        {
+            ushort crc = 0;
+            foreach (var oneByte in bytes)
+            {
+                var index = (byte) (crc ^ oneByte);
+                crc = (ushort) ((crc >> 8) ^ _table[index]);
+            }
+            return crc;
+        }
+
+        public byte[] ComputeChecksumBytes(byte[] bytes)
+        {
+            var crc = ComputeChecksum(bytes);
+            return BitConverter.GetBytes(crc);
+        }
+
+        public Crc16()
+        {
+            for (ushort i = 0; i < _table.Length; ++i)
+            {
+                ushort value = 0;
+                var temp = i;
+                for (byte j = 0; j < 8; ++j)
+                {
+                    if (((value ^ temp) & 0x0001) != 0)
+                    {
+                        value = (ushort) ((value >> 1) ^ Polynomial);
+                    }
+                    else
+                    {
+                        value >>= 1;
+                    }
+                    temp >>= 1;
+                }
+                _table[i] = value;
+            }
+        }
+    }
+}
