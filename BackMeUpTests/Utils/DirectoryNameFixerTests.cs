6371c976720fbfa2a45ef5d6499ee83df2f44cd1
Qwx <qwxmail@gmail.com>
2015-01-05 20:31:33 +0200

    Fixing up git

---
+++ BackMeUpTests/Utils/DirectoryNameFixerTests.cs
@@ -0,0 +1,54 @@
+﻿using BackMeUp.Utils;
+using NUnit.Framework;
+
+namespace BackMeUpTests.Utils
+{
+    [TestFixture]
+    public class DirectoryNameFixerTests
+    {
+        public static DirectoryNameFixer MakeDirectoryNameFixer()
+        {
+            return new DirectoryNameFixer();
+        }
+
+        [Test]
+        public void ReplaceInvalidCharacters_GetsGoodPath_ReturnsGoodPath()
+        {
+            var directoryNameFixer = MakeDirectoryNameFixer();
+
+            var result = directoryNameFixer.ReplaceInvalidCharacters(@"What");
+
+            StringAssert.AreEqualIgnoringCase(@"What", result);
+        }
+
+        [Test]
+        public void ReplaceInvalidCharacters_GetsDirectoryWithChar31_ReturnsChar31ReplacedWithUnderscore()
+        {
+            var directoryNameFixer = MakeDirectoryNameFixer();
+
+            var result = directoryNameFixer.ReplaceInvalidCharacters(string.Format(@"What{0}No!",(char)31));
+
+            StringAssert.AreEqualIgnoringCase(@"What_No!", result);
+        }
+
+        [Test]
+        public void ReplaceInvalidCharacters_GetsDirectoryWithQuestionMark_ReturnsQuestionMarkReplacedWithUnderscore()
+        {
+            var directoryNameFixer = MakeDirectoryNameFixer();
+
+            var result = directoryNameFixer.ReplaceInvalidCharacters(@"What?\/:No!");
+
+            StringAssert.AreEqualIgnoringCase(@"What____No!", result);
+        }
+
+        [Test]
+        public void ReplaceInvalidCharacters_GetsNullDirectory_ReturnsNull()
+        {
+            var directoryNameFixer = MakeDirectoryNameFixer();
+
+            var result = directoryNameFixer.ReplaceInvalidCharacters(null);
+
+            StringAssert.AreEqualIgnoringCase(null, result);
+        }
+    }
+}
