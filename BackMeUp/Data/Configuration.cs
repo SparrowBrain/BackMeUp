cc96387519e7da29ea1f534c41831ecfdddeed12
Qwx <qwxmail@gmail.com>
2015-01-11 00:36:46 +0200

    Added NSubstitute
    Updated NUnitRunners

--- BackMeUp/Data/Configuration.cs
+++ BackMeUp/Data/Configuration.cs
@@ -3,6 +3,9 @@
     public class Configuration
     {
         public string BackupDirectory { get; set; }
+
+        // HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Uplay
+        // InstallLocation
         public string SaveGameDirectory { get; set; }
     }
 }
